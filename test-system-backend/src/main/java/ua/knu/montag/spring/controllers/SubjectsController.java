d64b63c1aed1eb89c6c6b7c548171bc0ae0a39fc
Marina Montag <montag.marinka@gmail.com>
2021-05-29 20:40:21 +0300

    send subject's tests

--- test-system-backend/src/main/java/ua/knu/montag/spring/controllers/SubjectsController.java
+++ test-system-backend/src/main/java/ua/knu/montag/spring/controllers/SubjectsController.java
@@ -3,6 +3,7 @@ package ua.knu.montag.spring.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ua.knu.montag.spring.model.Subject;
+import ua.knu.montag.spring.model.Test;
 import ua.knu.montag.spring.service.interfaces.SubjectService;
 
 import java.util.List;
@@ -20,5 +21,9 @@ public class SubjectsController {
     @GetMapping()
     public ResponseEntity<List<Subject>> getSubjects(){
         return ResponseEntity.ok().body(service.getSubjects());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Test>> getTests(@PathVariable long id){
+        return ResponseEntity.ok().body(service.getSubjectTests(id));
     }
 }
